fd427b588eac96cf0a3ec8d4c4411cca1006b4fe
nikita <iosivovichp@bk.ru>
2025-11-09 11:59:17 +0300

    remove api(1)

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('devices/', include('devices.urls')),
-    path('api/', include('api.urls')),
 ]
